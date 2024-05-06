646da1175237323e3fa4c87c7d4c7b2fe9f863e9
sajjad yami <sajjadyami77@gmail.com>
2024-05-06 23:42:24 +0330

    adding put in table and starter

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,22 @@
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         TTT ttt = new TTT();
-        ttt.initializer2();
-        ttt.printer();
+        ttt.initializer();
+        Scanner my_box = new Scanner(System.in);
+        Scanner my_character = new Scanner(System.in);
+        System.out.println("choose your number :");
+        int box = my_box.nextInt();
+        System.out.println("choose your character :");
+        String character = my_character.nextLine();
+        ttt.put_in_table(box, character.charAt(0));
+        ttt.put_in_table(2,'x');
+        ttt.put_in_table(3,'x');
         ttt.winner_check();
+        ttt.printer();
 
     }
 }
--- src/TTT.java
+++ src/TTT.java
@@ -1,5 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Scanner;
 
 public class TTT {
 
@@ -14,16 +13,9 @@ public class TTT {
         return table;
     }
 
-    public Character[][] initializer2() {
-        for (int i = 0; i < table.length; i++) {
-            for (int j = 0; j < table.length; j++) {
-                table[j][i] = 'x';
-            }
-        }
-        return table;
-    }
+    public boolean winner_check() {
 
-    public void winner_check() {
+        boolean check = false;
 
         StringBuilder line_r = new StringBuilder();
         StringBuilder line_c = new StringBuilder();
@@ -37,25 +29,65 @@ public class TTT {
             }
             if (line_r.toString().equals("xxx")) {
                 System.out.println("x winner");
+                check = true;
             }
             if (line_r.toString().equals("ooo")) {
                 System.out.println("o winner");
+                check = true;
             }
             if (line_c.toString().equals("xxx")) {
                 System.out.println("x winner");
+                check = true;
             }
             if (line_c.toString().equals("ooo")) {
                 System.out.println("o winner");
+                check = true;
             }
         }
         line_X1.append(table[0][0]).append(table[1][1]).append(table[2][2]);
         line_X2.append(table[0][2]).append(table[1][1]).append(table[2][0]);
         if (line_X1.toString().equals("xxx") || line_X2.toString().equals("xxx")) {
             System.out.println("x winner");
+            check = true;
         }
         if (line_X1.toString().equals("ooo") || line_X2.toString().equals("ooo")) {
             System.out.println("o winner");
+            check = true;
+        }
+        return check;
+    }
 
+    public void put_in_table(int number, char sign){
+        switch (number){
+            case 1:
+                table[0][0] = sign;
+                break;
+            case 2:
+                table[0][1] = sign;
+                break;
+            case 3:
+                table[0][2] = sign;
+                break;
+            case 4:
+                table[1][0] = sign;
+                break;
+            case 5:
+                table[1][1] = sign;
+                break;
+            case 6:
+                table[1][2] = sign;
+                break;
+            case 7:
+                table[2][0] = sign;
+                break;
+            case 8:
+                table[2][1] = sign;
+                break;
+            case 9:
+                table[2][2] = sign;
+                break;
+            default:
+                break;
         }
     }
 
@@ -68,5 +100,17 @@ public class TTT {
             }
             System.out.println("______________");
         }
+
+    public static void starter(){
+        TTT ttt = new TTT();
+        Scanner my_box = new Scanner(System.in);
+        Scanner my_character = new Scanner(System.in);
+        ttt.initializer();
+        ttt.printer();
+        while (!ttt.winner_check()){
+            //TODO
+        }
+    }
 }
+
 
